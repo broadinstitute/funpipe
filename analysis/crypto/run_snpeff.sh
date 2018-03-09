55d654b1d40b5f2efaf91b33e2d37af36edc1cb9
xiaolicbs <xiaoli.cbs@gmail.com>
2018-03-09 18:52:55 -0500

    bug fix

--- analysis/crypto/run_snpeff.sh
+++ analysis/crypto/run_snpeff.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-#$ -l h_rt=06:16:16
 #$ -N snpeff
-#$ -l h_vmem=11g
+#$ -l h_rt=06:16:16
+#$ -l h_vmem=80g
 #$ -pe smp 1
-#$ -binding linear:4
+#$ -binding linear:1
 #$ -R y
-#$ -P gscid
+#$ -q gscid
 #$ -m ea
 #$ -M xiaoli@broadinstitute.org
 #$ -o /gsap/garage-fungal/Crypto_neoformans_seroD_B454/analysis/JEC21
 #$ -j y
 #$ -wd /gsap/garage-fungal/Crypto_neoformans_seroD_B454/analysis/JEC21
 
+# -P gscid
+
+
 source /broad/software/scripts/useuse
 reuse UGER
-reuse GCC-5.2
 reuse Java-1.8
+reuse Python-3.4
 
 export PATH=/cil/shed/sandboxes/xiaoli/fungal-pipeline/src:$PATH
 
@@ -24,6 +27,6 @@ cd /gsap/garage-fungal/Crypto_neoformans_seroD_B454/analysis/JEC21
 run_snpeff.py \
   -i AFA_1003_15.vcf.gz \
   -o AFA_1003_15.snpeff.vcf.gz \
-  --ram 10 --genome_name JEC21 \
+  --ram 80 --genome_name JEC21 \
   -c /gsap/garage-fungal/Crypto_neoformans_seroD_B454/annotation/snpeff_db/snpEff.config \
-  --jar /gsap/garage-fungal/Crypto_neoformans_seroD_B454/annotation/snpeff_db/snpEff.jar
+  --snpeff_jar /gsap/garage-fungal/Crypto_neoformans_seroD_B454/annotation/snpeff_db/snpEff.jar
