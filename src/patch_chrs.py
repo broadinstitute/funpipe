c2389f2c68892030f6e5d6703c1d63a581b396d4
xiaolicbs <xiaoli.cbs@gmail.com>
2018-03-14 18:22:32 -0400

    remove MT from gff

--- src/patch_chrs.py
+++ src/patch_chrs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import re
 import gzip
-from pipeline import cd
+from pipeline import cd, samtools_index_fa
 
 '''
     Patch contig names in GFF and fasta from NCBI
@@ -40,20 +40,34 @@ def patch_fasta(in_fa, out_fa, rm_mt = True):
     return chr_map
 
 
-def patch_gff_contig(chr_map, in_gff, out_gff):
+def patch_gff_contig(chr_map, in_gff, out_gff, rm_mt=True):
+    '''
+    chr_map: a dictionary that maps
+    in_gff: input gff file
+    out_gff: output gff file
+    rm_mt: boolean, whether remove mitochondrion genes or not
+    '''
     with open(in_gff, 'r') as gff, open(out_gff, 'w') as pgff:
         for line in gff:
             if line[0] == '#':
                 if line[:5] == '##seq':
                     seq_region = line.split(' ')
                     seq_region[1] = chr_map[seq_region[1]]
-                    pgff.write(' '.join(seq_region))
+                    if rm_mt:
+                        if seq_region[1] != 'mitochondrion':
+                            pgff.write(' '.join(seq_region))
+                    else:
+                        pgff.write(' '.join(seq_region))
                 else:
                     pgff.write(line)
             else:
                 gff_line = line.split('\t')
                 gff_line[0] = chr_map[gff_line[0]]
-                pgff.write('\t'.join(gff_line))
+                if rm_mt:
+                    if gff_line[0] != 'chrMT':
+                        pgff.write('\t'.join(gff_line))
+                else:
+                    pgff.write('\t'.join(gff_line))
 
 def write_chr_map(chr_map, out_tsv):
     with open(out_tsv, 'w') as out:
@@ -65,11 +79,12 @@ if __name__ == '__main__':
     cd('/gsap/garage-fungal/Crypto_neoformans_seroD_B454/assembly/NCBI_05Mar18')
     chr_map = patch_fasta(
         'GCF_000149245.1_CNA3_genomic.fna',
-        'GCF_000149245.1_CNA3_genomic.patched.noMT.fna',
+        'GCF_000149245.1_CNA3_genomic.patched.noMT.fasta',
         rm_mt=True)
+    samtools_index_fa('GCF_000149245.1_CNA3_genomic.patched.noMT.fasta')
     write_chr_map(chr_map, 'GCF_000149245.1_CNA3_chr_map.tsv')
     # patch contig name in the chr
     patch_gff_contig(
         chr_map,
         'GCF_000149245.1_CNA3_genomic.gff',
-        'GCF_000149245.1_CNA3_genomic.patched.gff')
+        'GCF_000149245.1_CNA3_genomic.patched_noMT.gff')
