bf5eb181ff1bef22105da3cca65ca539310a131b
xiaolicbs <xiaoli.cbs@gmail.com>
2018-03-18 10:55:40 -0400

    generalize regex, modulize

--- src/patch_chrs.py
+++ src/patch_chrs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
 import gzip
+import sys
 from pipeline import cd, samtools_index_fa
 
 '''
@@ -8,13 +9,29 @@ from pipeline import cd, samtools_index_fa
     author: Xiao Li (xiaoli@broadinstitute.org)
 '''
 
-def patch_fasta(in_fa, out_fa, rm_mt = True):
+def chr_map_from_desc(desc_line):
+    ''' extract contig and chromosome name from description line '''
+    if 'mitochondrion' in desc_line:
+        is_mt = True
+        chr = re.sub(r'mitochondrion', 'chrMT', chr)
+    elif 'chromosome':
+        is_mt = False
+        matched = re.match(r'>(\S+) .+ chromosome (\d+),.+', desc_line)
+        contig, chr = matched.group(1), matched.group(2)
+        if not chr:
+            raise ValueError("contig name not matched!")
+        chr = 'chr'+chr
+    else:
+        raise ValueError("Unrecognized description line:" + desc_line)
+    return (contig, chr)
+
+
+def patch_fasta(in_fa, out_fa, rm_mt=True):
     '''
     in_fa: input fasta file, can be gziped with suffix gz
     out_fa: output fasta file
     rm_mt: boolean, whether to remove mitochondrion sequence or not
     '''
-    is_mt = False
     chr_map = {}
     if in_fa[-2:] == 'gz':
         fa = gzip.open(in_fa, 'rt')
@@ -23,19 +40,12 @@ def patch_fasta(in_fa, out_fa, rm_mt = True):
     with open(out_fa, 'w') as pfa:
         for line in fa:
             if line[0] == '>':
-                matched = re.match(
-                    r'>(\S+) Cryptococcus neoformans var. grubii H99 (.+),.+',
-                    line)
-                contig, chr = matched.group(1), matched.group(2)
-                if 'mitochondrion' in line:
-                    is_mt = True
-                    chr = re.sub(r'mitochondrion', 'chrMT', chr)
-                else:
-                    is_mt = False
-                    chr = re.sub(r'chromosome ', 'chr', chr)
+                (contig, chr) = chr_map_from_desc(line)
                 chr_map[contig] = chr
                 line = '>'+chr+'\n'
-            if rm_mt and (not is_mt):
+            if rm_mt and (chr != 'chrMT'):
+                pfa.write(line)
+            else:
                 pfa.write(line)
     return chr_map
 
@@ -76,15 +86,11 @@ def write_chr_map(chr_map, out_tsv):
 
 
 if __name__ == '__main__':
-    cd('/gsap/garage-fungal/Crypto_neoformans_seroD_B454/assembly/NCBI_05Mar18')
+    cd('/gsap/garage-fungal/Crypto_neoformans_seroD_B454/assembly/NCBI_JEC21')
+    prefix = 'GCF_000091045.1_ASM9104v1_genomic'
     chr_map = patch_fasta(
-        'GCF_000149245.1_CNA3_genomic.fna',
-        'GCF_000149245.1_CNA3_genomic.patched.noMT.fasta',
-        rm_mt=True)
-    samtools_index_fa('GCF_000149245.1_CNA3_genomic.patched.noMT.fasta')
-    write_chr_map(chr_map, 'GCF_000149245.1_CNA3_chr_map.tsv')
+        prefix+'.fna.gz', prefix+'.patched.noMT.fasta', rm_mt=True)
+    samtools_index_fa(prefix+'.patched.noMT.fasta')
+    write_chr_map(chr_map, prefix+'_chr_map.tsv')
     # patch contig name in the chr
-    patch_gff_contig(
-        chr_map,
-        'GCF_000149245.1_CNA3_genomic.gff',
-        'GCF_000149245.1_CNA3_genomic.patched_noMT.gff')
+    patch_gff_contig(chr_map, prefix+'.gff', prefix+'.patched_noMT.gff')
