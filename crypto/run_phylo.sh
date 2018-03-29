4e686e434bd56f5ae92aec3f34adb23cc383dfa3
xiaolicbs <xiaoli.cbs@gmail.com>
2018-03-29 19:47:11 -0400

    phylo pipeline

--- crypto/run_phylo.sh
+++ crypto/run_phylo.sh
@@ -20,11 +20,17 @@ reuse Python-2.7
 
 PATH=/cil/shed/apps/internal/OBA/SNPs/Analysis:$PATH
 PATH=/cil/shed/apps/external/phylogeny/FastTree-2.1.8:$PATH
-export $PATH
+export PATH=/cil/shed/apps/internal/OBA/SNPs:$PATH
+
+prefix=crypto_serod_pilot.snp
 
 cd /gsap/garage-fungal/Crypto_neoformans_seroD_B454/analysis/JEC21_NCBI
+cp /gsap/store/cromwell_executions/gatk/72f2438d-bda1-4ad8-9bcc-c941246141a6/call-FilterSnps/execution/filtered_SNPs.vcf ${prefix}.vcf
 
-prefix=crypto_serod_pilot
-echo "${prefix}.vcf" > vcf_list.txt
+filterGatkGenotypes.py \
+  --min_GQ 50 --min_percent_alt_in_AD 0.8 --min_total_DP 10 \
+   ${prefix}.vcf > ${prefix}_GQ50_AD08_DP10.vcf
+
+echo ${prefix}_GQ50_AD08_DP10.vcf > vcf_list.txt
 vcfSnpsToFasta.py vcf_list.txt > ${prefix}.fasta
 FastTreeDP -nt ${prefix}.fasta > ${prefix}.nwk
