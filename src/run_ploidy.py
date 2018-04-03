a0528950fa5d8c60e10002c103e26ec280a94e92
xiaolicbs <xiaoli.cbs@gmail.com>
2018-04-03 15:19:08 -0400

    bug fix

--- src/run_ploidy.py
+++ src/run_ploidy.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
-import os
+from os.path import basename, splitext
 import sys
 import argparse
-from pipeline import bam_depth, depth_per_window, sort_bam, cd
+from pipeline import bam_depth, depth_per_window, sort_bam, cd, run
 
-def run_ploidy(bam, faidx):
-    sorted_bam = sort_bam(bam)
-    pileup = bam_depth(sorted_bam)
-    depth_per_window(pileup, os.path.basename(bam), faidx)
-    # rm(sorted_bam)
+
+def run_ploidy(out_dir, bam, faidx):
+    with cd(args.out_dir):
+        sorted_bam = sort_bam(bam)
+        pileup = bam_depth(sorted_bam)
+        depth_per_window(pileup, basename(bam), faidx)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='run ploidy analysis by analysing ')
+        description='Run ploidy analysis by analysing ')
     # required arguments
-    required = parser.add_argument_group('required arguments')
+    required = parser.add_argument_group('Required arguments')
     required.add_argument(
-        '-i', '--bam', required=True, help='Input file')
+        '-i', '--bam', required=True, help='Input BAM path')
     required.add_argument(
         '--faidx', required=True, help='Fasta index')
+
     # optional arguments
-    parser.add_argument('--prefix', help='Prefix of output files')
     parser.add_argument(
         '-o', '--out_dir', help="Output file", default='.')
 
     args = parser.parse_args()
-
-    with cd(args.out_dir):
-        run_ploidy(args.bam, args.faidx)
+    run_ploidy(args.out_dir, args.bam, args.faidx)
