a2ccf794f719f456d23ee91aead238ab3cc0a8e3
xiaolicbs <xiaoli.cbs@gmail.com>
2018-04-03 16:01:45 -0400

    pep8 style fix

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,60 +1,82 @@
 from subprocess import check_call
 import os
+import sys
 import contextlib
-
 # from plumbum import local
 # from plumbum.cmd import wget
 # import configparser
 
+
 @contextlib.contextmanager
 def cd(dir):
+    ''' change directory
+    :param dir: new directory to change to
+    '''
     original_path = os.getcwd()
     os.chdir(dir)
     yield
     os.chdir(original_path)
 
+
 def run(cmd):
+    ''' execute a specific command
+    :param cmd: command to execute
+    '''
+    sys.stderr.write(cmd+"\n")
     check_call(cmd, shell=True)
+    return 0
+
 
 def rm(file):
+    ''' remove a file
+    :param file: path of file to remove
+    '''
     run('rm '+file)
+
 
 def index_fa(fa):
     ''' index fasta file with common genomic tools
     :param fa: fasta file
+    :returns: None
     '''
     samtools_index_fa(fa)
     bwa_index_fa(fa)
     pcd = picard()
     pcd.dict(fa)
 
-def sort_bam(bam, out_dir='.', delete=False):
+
+def sort_bam(bam, out_dir='.'):
     ''' sort BAM using samtools
     :param bam: input bam
     :param out_dir: output directory, default '.'
     :param delete: delete original BAM
+
+    :returns: name of sorted bam
     '''
     bam_name = os.path.basename(bam)
     outfile = os.path.join(
         out_dir, os.path.splitext(bam_name)[0] + '.sorted.bam')
     run('samtools sort '+bam+' > '+outfile)
-    if delete:
-        rm(bam)
-    return output
+    return outfile
+
 
 def samtools_index_fa(fa):
     ''' index a fasta file in place
     :param fa: fasta file
+    :returns: index file name
     '''
     run('samtools faidx '+fa)
     return fa+'.fai'
 
+
 def bwa_align(fa, fq1, fq2, prefix):
     ''' perform bwa alignment
-        fa: fasta file
-        fq1: fq file1
-        fq2: fq file2
-        prefix: output file prefix
+    :param fa: fasta file
+    :param fq1: fq file1
+    :param fq2: fq file2
+    :param prefix: output file prefix
+
+    :returns:
     '''
     if not os.path.isfile(fa+'.fai'):
         samtools_index_fa(fa)
@@ -66,18 +88,20 @@ def bwa_align(fa, fq1, fq2, prefix):
     index_bam(sorted_bam)
     return sorted_bam
 
+
 def bam2fqs(bam, prefix, ram, picard):
     ''' realign BAM file to a reference genome
         bam: bam file
         prefix: output prefix
         fa: fasta file
     '''
-    fq1=prefix+'_1.fq.gz'
-    fq2=prefix+'_2.fq.gz'
+    fq1 = prefix+'_1.fq.gz'
+    fq2 = prefix+'_2.fq.gz'
     cmd = ' '.join(['java -Xmx'+str(ram)+'g', '-jar', picard, 'SamToFastq',
                     'I='+bam, 'F='+fq1, 'F2='+fq2])
     run(cmd)
     return (fq1, fq2)
+
 
 def pilon(fa, bam, prefix, ram, threads, jar):
     '''
@@ -99,6 +123,7 @@ def pilon(fa, bam, prefix, ram, threads, jar):
     run(cmd)
     return cmd
 
+
 def process_pilon_out(log, outdir, prefix):
     ''' process pilon output
         log: logfile
@@ -109,13 +134,16 @@ def process_pilon_out(log, outdir, prefix):
     run(cmd)
     return cmd
 
+
 def index_bam(bam):
     run('samtools index '+bam)
     return bam+'.bai'
 
+
 def bwa_index_fa(fa):
     run('bwa index '+fa)
     return fa+'.fai'
+
 
 def snpeff(invcf, outvcf, jar, config, genome, ram):
     ''' run SNPEFF on a vcf
@@ -136,8 +164,8 @@ def snpeff(invcf, outvcf, jar, config, genome, ram):
     run(cmd)
     return cmd
 
-def snpeff_db(
-    gff3, dir, genome, config, prefix, ram, jar, ref_fa):
+
+def snpeff_db(gff3, dir, genome, config, prefix, ram, jar, ref_fa):
     ''' Create snpEff database
     gff3: gff file of gene annotation
     genome: name of the reference genome
@@ -153,6 +181,7 @@ def snpeff_db(
     run(cmd)
     return cmd
 
+
 def bam_depth(bam, out_prefix, idx=False):
     ''' calculate bam depth
     :param bam: input to bam file
@@ -160,14 +189,15 @@ def bam_depth(bam, out_prefix, idx=False):
     :param idx: output index
     '''
     outfile = out_prefix+'.depth.gz'
-    cmd = 'samtools depth '+bam+' | bgzip > '+ outfile
+    cmd = 'samtools depth '+bam+' | bgzip > '+outfile
     run(cmd)
     if idx:
         tabix(outfile, type='vcf')
     return outfile
 
+
 def depth_per_window(pileup, out_prefix, faidx, window=5000):
-    '''
+    ''' calculate depth per window
     :param pileup: pileup file from samtools
     :param window: window size in basepair
     '''
@@ -179,6 +209,7 @@ def depth_per_window(pileup, out_prefix, faidx, window=5000):
     run(cmd)
     return cmd
 
+
 def tabix(file, type=None):
     ''' Index tabix file
     :param file: input file
@@ -189,6 +220,7 @@ def tabix(file, type=None):
         cmd += ' -p '+type
     run(cmd)
 
+
 def fa2phylip(fa, out_prefix, jar):
     ''' transfer fasta file to phylip with java tool readSeq
     :param fa: fasta file
@@ -198,6 +230,7 @@ def fa2phylip(fa, out_prefix, jar):
     cmd = ' '.join(['java -cp', jar, 'run -f 12', fa])
     run(cmd)
     return
+
 
 def ramxl(phylip, output, threads):
     ''' Run RAaML
@@ -211,6 +244,7 @@ def ramxl(phylip, output, threads):
     run(cmd)
     return output
 
+
 def fasttree(fa, prefix):
     ''' Run FastTreeDP
     :param fa: fasta file
@@ -221,6 +255,7 @@ def fasttree(fa, prefix):
     ])
     return prefix+'.nwk'
 
+
 def vcfsnpsToFa(vcf_list, out_prefix):
     out_file = out_prefix+'.fasta'
     cmd = ' '.join([
@@ -228,6 +263,7 @@ def vcfsnpsToFa(vcf_list, out_prefix):
     ])
     run(cmd)
     return out_file
+
 
 def filterGatkGenotypes(vcf, out_prefix):
     ''' filter Gatk output vcf
@@ -256,6 +292,20 @@ class picard:
         cmd = ' '.join([self.cmd, "R="+fa, "O="+dict])
         run(cmd)
 
+    def bam2fqs(bam, prefix, ram, picard):
+        ''' Realign BAM file to a reference genome
+        :param bam: bam file
+        :param prefix: output prefix
+        :parma fa: fasta file
+        :returns: tuple of fq file pairs
+        '''
+        fq1 = prefix+'_1.fq.gz'
+        fq2 = prefix+'_2.fq.gz'
+        cmd = ' '.join([self.cmd, 'SamToFastq', 'I='+bam, 'F='+fq1, 'F2='+fq2])
+        run(cmd)
+        return (fq1, fq2)
+
+
 class gatk:
     def __init__(self, fa, jar='/xchip/gtex/xiaoli/tools/GenomeAnalysisTK.jar',
                  out_dir='.', RAM=4):
@@ -272,8 +322,8 @@ class gatk:
             'java -Xmx'+str(RAM)+'g -jar', jar, '-R', fa
         ])
 
-    def variantEval(
-        self, vcf, prefix, titv=True, samp=True, indel=True, multi=True):
+    def variantEval(self, vcf, prefix, titv=True, samp=True, indel=True,
+                    multi=True):
         ''' VCF sample QC by different stratifications
         :param vcf: input vcf
         :param prefix: output prefix
@@ -284,7 +334,7 @@ class gatk:
         '''
         out = os.path.join(self.out_dir, prefix+'.eval')
         cmd = ' '.join([self.cmd, '-T VariantEval', '--eval', vcf,
-            '-o', out, '-noEV -noST -EV CountVariants'])
+                        '-o', out, '-noEV -noST -EV CountVariants'])
         if titv:
             cmd += ' -EV TiTvVariantEvaluator'
         if samp:
