e2e64e1ec1dfc08b301817caf7de9353ad576670
xiaolicbs <xiaoli.cbs@gmail.com>
2018-07-06 16:37:32 -0400

    add unittest for coverage merging

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,6 +4,7 @@ import os
 from subprocess import check_call
 
 from patch_ref_contigs import *
+from post_process_coverage import get_contig_sets
 import filecmp
 
 # class TestMethod(unittest.TestCase):
@@ -35,6 +36,17 @@ class TestPatchRefContigs(unittest.TestCase):
             filecmp.cmp('expNoMt.gff', 'outNoMt.gff'),
             'Output file not same as expected.')
 
+
+class TestGetContigSets(unittest.TestCase):
+    def setUp(self):
+        self.all_contigs = {'1_A', '2_A', '1_D', '2_D', '1_B', '2_B'}
+        self.subgenome = ['_A', '_B', '_D']
+
+    def test_get_contig_sets(self):
+        contig_map = get_contig_sets(self.all_contigs, self.subgenome)
+        expect_out = {'1_A': 1, '2_A': 2, '1_B': 3, '2_B': 4, '1_D': 5,
+                      '2_D': 6}
+        self.assertDictEqual(contig_map, expect_out)
 # test combineVariants: java -jar /xchip/gtex/xiaoli/tools/GenomeAnalysisTK.jar --variant test.vcf --variant test2.vcf -o merged.vcf -genotypeMergeOptions UNSORTED -R test.fa -T CombineVariants --assumeIdenticalSamples
 
 
