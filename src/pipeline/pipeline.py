4ddcf48aaf5dc15a6f7e73c2f9994a76eef0dc08
xiaolicbs <xiaoli.cbs@gmail.com>
2018-07-23 18:56:24 -0400

    sv breakdancer

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -480,6 +480,21 @@ def clean_bam(bam, out_prefix):
     run(cmd)
     return out_file
 
+# structural variation
+
+
+def breakdancer(bam_file, prefix):
+    ''' Detect structural variation using breakdancer
+    :param bam_file: input bam file
+    :param prefix: output prefix
+    :return
+    '''
+    # create config files
+    cfg_file = prefix+'.cfg'
+    run('bam2cfg.pl -g -h'+bam_file+'> '+prefix+'.cfg')
+    # Detect chromosomal structural variants using breakdancer-max
+    run('brakdancer-max -q 40 -r 20 -y 90 '+cfg_file)
+    return
 
 # def get_ref(ftp, md5, dir='.'):
 #     """ download reference files from NCBI and perform md5sumcheck to files
