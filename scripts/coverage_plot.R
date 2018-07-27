d62a5f7eb2d304fccf25d4abf85413de34b9b466
Xiao Li <xiaoli@vpn5-169.broadinstitute.org>
2018-07-27 06:23:50 -0400

    fix #15

--- scripts/coverage_plot.R
+++ scripts/coverage_plot.R
@@ -38,13 +38,13 @@ mid_pos <- function(pos_vec) {
 #' @description
 #' @param cov coverage data.frame
 #' @param sample sample name
-#' @param col_vec vector of colors for each chromosome
+#' @param cols vector of colors for each chromosome
 #' @param contigs contigs need to plot out
 #' @param inverse_contig list of contigs need to reverse their physical postion
 #' @param is_D: whether the subgenome is a D subgenome
 #'
 cov_plot <- function (
-  cov, sample, col_vec, contigs=NULL, inverse_contig=NULL, is_D=F) {
+  cov, sample, cols, contigs=NULL, inverse_contigs=NULL, is_D=F) {
   cov1 <- cov[cov$chr %in% contigs, ]
   min_pos = min(cov1$pos)
   max_pos = max(cov1$pos)
@@ -77,78 +77,85 @@ cov_plot <- function (
   return(1)
 }
 
+
+coverage_plot <- function(density, prefix, color_csv, legacy) {
+  if (legacy) {
+    start_column = 6
+  } else{
+    start_column = 2
+  }
+  
+  cov <- read.csv(density, sep='\t')
+  n_sample = dim(cov)[2] - start_column
+  
+  color <- read.csv(color_csv, sep='\t', 
+                    col.names=c('chr', 'start', 'end', 'R', 'G', 'B'), header=F
+  )
+  cols <- vector(length=28)
+  for (i in 1:dim(color)[1]) {
+    cols[i] <- rgb(color$R[i], color$G[i], color$B[i])
+  }
+  
+  # separate two sub-genomes
+  subg1 <- c( 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14)    # A
+  subg2 <- c(15, 16, 25, 26, 18, 19, 20, 21, 23, 24, 17, 27, 28, 22)  # D
+  
+  cov = reorder_chr(cov, c(subg1, subg2))
+  cols <- vector(length=28)
+  color$chr = c(subg1, subg2)
+  for (i in 1:dim(color)[1]) {
+    cols[color$chr[i]] <- rgb(color$R[i], color$G[i], color$B[i])
+  }
+  
+  # inverse
+  inverse_contigs <- c(3, 4, 5, 7, 8, 10, 14)
+  png(paste0(prefix,'.cmp.png'), width=2500, height=n_sample*420, unit='px',
+      res=300)
+  
+  par(mfrow=c(n_sample * 2, 1))
+  for (sample in names(cov)[(1+start_column):(n_sample+start_column)]) {
+    print(sample)
+    par(mar=c(1.5, 5, 1, 0.2))
+    cov_plot(cov, sample, cols, subg1, inverse_contigs)
+    cov_plot(cov, sample, cols, subg2, is_D=T)
+  }
+  dev.off()
+  
+}
+
 parser <- ArgumentParser(description='Generate coverage plot')
 parser$add_argument('-d', '--density', help='input density tsv file')
-parser$add_argument('-c', '--color', help='color file for each chromosomes')
-parser$add_argument('-i', '--inverse_contigs',
-                    help='contigs that need to inverse positions to complment strand')
 parser$add_argument('-p', '--prefix', help='output prefix')
-parser$add_argument('-l', '--legacy', help='legacy mode'
+parser$add_argument('-l', '--legacy', help='legacy mode', action='store_true')
+parser$add_argument('-c', '--color', help='color file for each chromosomes')
+
+# parser$add_argument('-i', '--inverse_contigs',
+#                     help='contigs that need to inverse positions to complment strand')
 args <- parser$parse_args()
 
 # add option to process
 
-if (legacy) {
-  start_column = 6
-} else{
-  start_column = 2
-}
-
-cov <- read.csv(density, sep='\t')
-n_sample = dim(cov)[2] - start_column
-
-color <- read.csv(
-   '/cil/shed/sandboxes/xiaoli/cryptoserod/refs/cneo_h99_jec21_chr_coord_5k.dat',
-   sep='\t', col.names=c('chr', 'start', 'end', 'R', 'G', 'B'), header=F
-)
-cols <- vector(length=28)
-for (i in 1:dim(color)[1]) {
-  cols[i] <- rgb(color$R[i], color$G[i], color$B[i])
-}
-
-# separate two sub-genomes
-subg1 <- c( 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14)    # A
-subg2 <- c(15, 16, 25, 26, 18, 19, 20, 21, 23, 24, 17, 27, 28, 22)  # D
-
-cov = reorder_chr(cov, c(subg1, subg2))
-cols <- vector(length=28)
-color$chr = c(subg1, subg2)
-for (i in 1:dim(color)[1]) {
-  cols[color$chr[i]] <- rgb(color$R[i], color$G[i], color$B[i])
-}
-
-
-
-# inverse
-inverse_contigs <- c(3, 4, 5, 7, 8, 10, 14)
-png(paste0(prefix,'.cmp.png'), width=2500, height=n_sample*420, unit='px',
-    res=300)
-par(mfrow=c(n_sample * 2, 1))
-for (sample in names(cov)[(1+start_column):(n_sample+start_column)]) {
-  print(sample)
-  par(mar=c(1.5, 5, 1, 0.2))
-  cov_plot(cov, subg1, sample, cols, inverse_contigs)
-  cov_plot(cov, subg2, sample, cols, is_D=T)
-}
-dev.off()
+coverage_plot(args$density, args$prefix, args$color, args$legacy)
 
 #
-contigs = c(paste('chr', 1:14, '_A', sep=''), paste('chr', 1:14, '_D', sep=''))
-pct_cov = read.csv('batch1_AD_progeny.pct_cov.tsv', sep='\t')
-pct_cov = read.csv('batch1_81D_AD_cov_fc.pct_cov.tsv', sep='\t')
-pct_cov = read.csv('batch1_15AD_cov.pct_cov.tsv', sep='\t')
-
-prefix='batch1_15D'
-pct_cov = pct_cov[order(match(pct_cov$contigs, contigs)),]
-n_samples = dim(pct_cov)[2] - 1
-png(paste(prefix,'cov.png', sep=''), width=3000, height=n_samples*500,
-    unit='px', res=300)
-par(mfrow=c(n_samples, 2))
-par(mar=c(2, 4, 2, 0.5))
-for (sample in names(pct_cov)[2:n_samples]) {
-  barplot(pct_cov[c(1:14), sample], names.arg=pct_cov$contigs[1:14],
-          main=paste(sample, '_A', sep=''), ylab='% coverage', ylim=c(0,6))
-  barplot(pct_cov[c(15:28), sample], names.arg=pct_cov$contigs[15:28],
-          main = paste(sample, '_D', sep=''), ylab='% coverage', ylim=c(0,6))
+if (FALSE) {
+  contigs = c(paste('chr', 1:14, '_A', sep=''), paste('chr', 1:14, '_D', sep=''))
+  pct_cov = read.csv('batch1_AD_progeny.pct_cov.tsv', sep='\t')
+  pct_cov = read.csv('batch1_81D_AD_cov_fc.pct_cov.tsv', sep='\t')
+  pct_cov = read.csv('batch1_15AD_cov.pct_cov.tsv', sep='\t')
+  
+  prefix='batch1_15D'
+  pct_cov = pct_cov[order(match(pct_cov$contigs, contigs)),]
+  n_samples = dim(pct_cov)[2] - 1
+  png(paste(prefix,'cov.png', sep=''), width=3000, height=n_samples*500,
+      unit='px', res=300)
+  par(mfrow=c(n_samples, 2))
+  par(mar=c(2, 4, 2, 0.5))
+  for (sample in names(pct_cov)[2:n_samples]) {
+    barplot(pct_cov[c(1:14), sample], names.arg=pct_cov$contigs[1:14],
+            main=paste(sample, '_A', sep=''), ylab='% coverage', ylim=c(0,6))
+    barplot(pct_cov[c(15:28), sample], names.arg=pct_cov$contigs[15:28],
+            main = paste(sample, '_D', sep=''), ylab='% coverage', ylim=c(0,6))
+  }
+  dev.off()
 }
-dev.off()
