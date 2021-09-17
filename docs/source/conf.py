f99079677260f57b0b31bef573c3d83331c3e67c
SizheQiu <siq001@DESKTOP-OL261DO.localdomain>
2021-09-17 12:08:39 +0800

    conf_change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('../../funpipe')
+sys.path.insert(0, os.path.abspath('../../funpipe'))
 
 # -- Project information -----------------------------------------------------
 
